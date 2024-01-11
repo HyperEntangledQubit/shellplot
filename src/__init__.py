d87991c24d6f8cf07ed963680aac7cf3a92be7a0
Brad <25336193+HyperEntangledQubit@users.noreply.github.com>
2024-01-10 21:16:49 -0600

    Updating module information and adding tests/
    Summary
    -------
    Various fixes to pyproject.toml and addition of test/ directory.
    
    Fixes #24

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,13 @@
 # SPDX-License-Identifier: Apache-2.0
 """Terminal agnostic plotting tool"""
+from __about__ import __name__, __version__, __author__, __description__
+
 __all__  = [
         "__about__",
+        "__author__",
+        "__description__",
+        "__name__",
         "__version__",
         ]
+
+
